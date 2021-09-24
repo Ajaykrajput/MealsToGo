f04aa73dab2bf87ea0dd9e86e4a1e5a200ce50e5
Ajay <ajayrajput7rcr@gmail.com>
2021-09-24 20:58:39 +0530

    restaurants added on map

---
+++ src/features/map/components/map-callout.component.js
@@ -0,0 +1,5 @@
+import React from "react";
+import { CompactRestaurantInfo } from "../../../componants/restaurant/compact-restaurant-info.componant";
+export const MapCallout = ({ restaurant }) => {
+  return <CompactRestaurantInfo restaurant={restaurant} />;
+};
